312a24a15ca34baf54cb89803218ea8d990969df
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-01-26 13:55:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 var isDate = function (input) {
   //   write your code here
+	if(input instanceof Date){
+		return !isNaN(input.getTime());
+	}
+
+	const parseDate = new Date(input);
+
+	return !isNaN(parseDate.getTime());
 };
 
 // Do not change the code below.
